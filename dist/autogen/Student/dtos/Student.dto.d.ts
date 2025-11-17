5a2d1c03f0f7d4631c5dec9110f8948f53283970
Apso <apso-bot@mavric.com>
2025-11-17 11:51:15 +0000

    Auto-generate database migrations

---
+++ dist/autogen/Student/dtos/Student.dto.d.ts
@@ -0,0 +1,15 @@
+export declare class StudentCreate {
+    first_name: string;
+    last_name: string;
+    email: string;
+    enrollment_date: string;
+    status: string;
+}
+export declare class Student {
+    id: number;
+    first_name: string;
+    last_name: string;
+    email: string;
+    enrollment_date: string;
+    status: string;
+}
---
+++ dist/migrations/1763380250595-generated-migration.d.ts
@@ -0,0 +1,6 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+export declare class GeneratedMigration1763380250595 implements MigrationInterface {
+    name: string;
+    up(queryRunner: QueryRunner): Promise<void>;
+    down(queryRunner: QueryRunner): Promise<void>;
+}
